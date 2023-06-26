962dc9925c08e474370bf6f6d3e25aceb6364060
Juuso <chipfrog@pop-os.localdomain>
2023-06-27 01:52:02 +0300

    add sidepanel for settings

--- renderer.js
+++ renderer.js
@@ -1,15 +1,25 @@
+let settingsOn = false
 
-    document.getElementById('download-btn').addEventListener('click', async () => {
-        let formData = new FormData(document.getElementById('downloadForm'))
-        let url = formData.get('url')
-        const response = await window.electronAPI.downloadVideo(url)
-        console.log(response)
-    })
 
-    document.getElementById('target-btn').addEventListener('click', async () => {
-        // let formData = new FormData(document.getElementById('downloadForm'))
-        const response = await window.electronAPI.setTargetDirectory()
-        console.log(response)
-    })
+document.getElementById('download-btn').addEventListener('click', async () => {
+    let formData = new FormData(document.getElementById('downloadForm'))
+    let url = formData.get('url')
+    const response = await window.electronAPI.downloadVideo(url)
+    console.log(response)
+})
+
+document.getElementById('target-btn').addEventListener('click', async () => {
+    const response = await window.electronAPI.setTargetDirectory()
+    console.log(response)
+})
+
+document.getElementById('settings-btn').addEventListener('click', async () => {
+    if (settingsOn) {
+        document.getElementById('sidepanel').style.width = "0"
+    } else {
+        document.getElementById('sidepanel').style.width = "250px"
+    }
+    settingsOn = !settingsOn
+})
 
 
